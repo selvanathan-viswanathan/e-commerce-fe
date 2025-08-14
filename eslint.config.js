b6398740827573af4b13a904adebe6ffca95aa5c
selvanathan-viswanathan <selvanathaan@gmail.com>
2025-08-14 13:38:14 +0530

    update the prettierrc

--- eslint.config.js
+++ eslint.config.js
@@ -5,40 +5,19 @@ import { globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  [
-    globalIgnores(
-      [
-        "dist",
-      ],
-    ),
-    {
-      files:
-        [
-          "**/*.{ts,tsx}",
-        ],
-      extends:
-        [
-          js
-            .configs
-            .recommended,
-          tseslint
-            .configs
-            .recommended,
-          reactHooks
-            .configs[
-            "recommended-latest"
-          ],
-          reactRefresh
-            .configs
-            .vite,
-        ],
-      languageOptions:
-        {
-          ecmaVersion: 2020,
-          globals:
-            globals.browser,
-        },
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  ],
-);
+  },
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig(
-  {
-    server:
-      {
-        port: 3000,
-        host: true, // allows access from Docker container
-      },
-    plugins:
-      [
-        react(),
-      ],
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true, // allows access from Docker container
   },
-);
+  plugins: [react()],
+});
